828b8d75f7c827cb18e9569642a7423b0f21b0cd
saitoudai587 <saitoudai587@gmail.com>
2026-04-13 13:09:49 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("fetch", (event) => {
   // Share Target の POST を受け取る
   if (event.request.method === "POST" &&
-      new URL(event.request.url).pathname === "/share.html") {
+      new URL(event.request.url).pathname === "/test/share.html") {
     event.respondWith(Response.redirect("/test/share.html"));
   }
 });
