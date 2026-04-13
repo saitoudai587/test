2c5adb86148eefe63bfa17e03b9a92bf968e26a5
saitoudai587 <saitoudai587@gmail.com>
2026-04-13 14:26:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
-// v3
+// v4
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
 
   if (event.request.method === "POST" &&
       url.pathname === "/test/share.html") {
-    event.respondWith(Response.redirect("/test/share.html"));
-    return;
+
+    event.respondWith((async () => {
+      const formData = await event.request.formData();
+      const file = formData.get("file"); // ← 画像がここに入る
+
+      const cache = await caches.open("shared-files");
+      await cache.put("/test/shared-image", new Response(file));
+
+      return Response.redirect("/test/share.html");
+    })());
   }
 });
